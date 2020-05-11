ed69bdd194dbaafd95d69e03be9b5b797a6fd624
JRTEC <jrtec@JRtecs-MacBook-Pro.local>
2020-05-10 22:54:58 -0500

    Add examples

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # keystonejs_test
+
+yarn create keystone-app my-app
+yarn dev
+
+admin@example.com
+ba0577896136
+
+https://medium.com/@victor36max/how-to-build-a-react-driven-blog-with-next-js-and-keystonejs-cae3cd9fb804
---
+++ todo/index.js
@@ -0,0 +1,32 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { StaticApp } = require('@keystonejs/app-static');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+
+const PROJECT_NAME = 'todo';
+const adapterConfig = { mongoUri: 'mongodb+srv://dbUser:dbPassword@cluster0-ahelj.mongodb.net/test' };
+
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter(adapterConfig),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new StaticApp({ path: '/', src: 'public' }),
+    new AdminUIApp({ enableDefaultRoute: true }),
+  ],
+};
